b74439310f8db802655579bb547a8d1cdf7ba02f
alexzerocod24 <ustvich.alex@gmail.com>
2024-10-24 23:18:14 +0300

    new code number in new-test

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-text_1 = ("Начало") # создание переменной
-text_2 = ("урока")
+number_1 = input("введите первое число")
+number_1 = int(number_1)
+number_2 = input("введите второе число")
+number_2 = int(number_2)
+print(number_1 + number_2)
+text_1 = ("Начало")
+text_2 = (" урока")
+reult = text_1 + text_2
+print(result)
 
